30af641fc848c5a89194c02f7cdf127e9c8d654f
termOS <olegmanko479@gmail.com>
2026-03-07 14:56:00 +0200

    Implement full assignment logic and JSON output
    Added utility functions for sorting, sum, duplicate removal, and JSON export. Updated main.cpp to process arrays, calculate sums, handle duplicates, and output results to e.txt and data.json. Project files updated to include JSON as a resource.

--- fucn.h
+++ fucn.h
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+// Сортування масиву у зростаючому порядку
 void sortAsc(vector<int>& v)
 {
 	for(int i = 0; i < v.size() - 1; i++)
@@ -21,4 +22,103 @@ void sortAsc(vector<int>& v)
 			}
 		}
 	}
+}
+
+// Сортування масиву у спадному порядку
+void sortDesc(vector<int>& v)
+{
+	for (int i = 0; i < v.size() - 1; i++)
+	{
+		for (int j = 0; j < v.size() - i - 1; j++)
+		{
+			if (v[j] < v[j + 1])
+			{
+				int temp = v[j];
+				v[j] = v[j + 1];
+				v[j + 1] = temp;
+			}
+		}
+	}
+}
+
+// Підрахунок суми елементів масиву
+int sumOfElements(vector<int>& v)
+{
+	int sum = 0;
+	for (int i = 0; i < v.size(); i++)
+	{
+		sum += v[i];
+	}
+	return sum;
+}
+
+// Функція для видалення однакових елементів з масиву, та переніс їх в інший масив
+
+void rmDuplicates(vector<int>& v, vector<int>& unique)
+{
+	for (int i = 0; i < v.size(); i++)
+	{
+		bool isDuplicate = false;
+		for (int j = 0; j < unique.size(); j++)
+		{
+			if (v[i] == unique[j])
+			{
+				isDuplicate = true;
+				break;
+			}
+		}
+		if (!isDuplicate)
+		{
+			unique.push_back(v[i]);
+		}
+	}
+}
+
+// Функція для виводу різниці масивів |V1 - V2| (елементи, які були видалені)
+void printDifference(vector<int>& v1, vector<int>& v2)
+{
+	for (int i = 0; i < v1.size(); i++)
+	{
+		int count = 0;
+
+		for (int j = 0; j < v1.size(); j++)
+		{
+			if (v1[i] == v1[j])
+				count++;
+		}
+
+		if (count > 1)
+			cout << v1[i] << " ";
+	}
+
+	cout << endl;
+}
+
+// Функція для запису результатів у файл json
+void JsonOut(ofstream& jout, int sum2, int sum3, int duplicatesCount, vector<int>& v1, vector<int>& v2)
+{
+	jout << "{\n";
+	jout << "  \"S2\": " << sum2 << ",\n";
+	jout << "  \"S3\": " << sum3 << ",\n";
+	jout << "  \"S3 - S2\": " << sum3 - sum2 << ",\n";
+	jout << "  \"duplicatesCount\": " << duplicatesCount << ",\n";
+	jout << "  \"V1\": [";
+    for (int i = 0; i < v1.size(); i++)
+    {
+        jout << v1[i];
+        if (i < v1.size() - 1) {
+            jout << ", ";
+        }
+    }
+    jout << "],\n";
+    jout << "  \"V2\": [";
+    for (int i = 0; i < v2.size(); i++)
+    {
+        jout << v2[i];
+        if (i < v2.size() - 1) {
+            jout << ", ";
+        }
+    }
+	jout << "]\n";
+	jout << "}\n";
 }
